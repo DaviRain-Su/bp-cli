94bfb1657f5899eb7ca44cbdb64fcf1a3e1c6e41
Davirain <davirain.yin@gmail.com>
2024-02-17 18:09:11 +0800

    Update trades.rs

--- crates/cli/src/command/public/trades.rs
+++ crates/cli/src/command/public/trades.rs
@@ -7,6 +7,16 @@ pub struct Trades {}
 
 impl Trades {
     pub async fn run(&self) -> anyhow::Result<()> {
-        todo!()
+        let config = get_config()?;
+        let bpx_client = BpxClient::init(config.base_url, &config.api_key, &config.api_secret)?;
+
+        let trade = bpx_client.get_recent_trades("SOL_USDC", None).await?;
+        println!("trade: {:?}", trade);
+
+        let history = bpx_client
+            .get_historical_trades("SOL_USDC", None, None)
+            .await?;
+        println!("history: {:?}", history);
+        Ok(())
     }
 }
